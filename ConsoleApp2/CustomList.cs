5f6c7d749daea1d37ae088bac5e37b79c3417a2e
Aysa <bd7hwd97w@code.edu.az>
2024-04-18 12:01:10 +0400

    ndncfj

---
+++ ConsoleApp2/CustomList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class CustomList<T> : IEnumerable<T>
+    {
+        private List<T> items;
+
+        public CustomList()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Find(Predicate<T> match)
+        {
+            return items.Find(match);
+        }
+
+        public List<T> FindAll(Predicate<T> match)
+        {
+            return items.FindAll(match);
+        }
+
+        public bool Remove(T item)
+        {
+            return items.Remove(item);
+        }
+
+        public int RemoveAll(Predicate<T> match)
+        {
+            return items.RemoveAll(match);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                CustomList<int> myList = new CustomList<int>();
+                myList.Add(1);
+                myList.Add(2);
+                myList.Add(3);
+
+                Console.WriteLine("Listedeki elemanlar:");
+                foreach (var item in myList)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("3'ü içeren elemanı tap:");
+                Console.WriteLine(myList.Find(x => x == 3));
+
+                Console.WriteLine("Tüm elemanları tap:");
+                foreach (var item in myList.FindAll(x => x > 1))
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("3'ü sil:");
+                myList.Remove(3);
+
+                Console.WriteLine("Listedeki elemanlar:");
+                foreach (var item in myList)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
